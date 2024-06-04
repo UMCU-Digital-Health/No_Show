be811ea91a5d9ec09aa403bb18e07d688fd216a3
EricWolters <e.j.wolters-4@umcutrecht.nl>
2024-06-04 08:23:21 +0200

    fixed notebook and tests

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,3 +1,5 @@
+from unittest.mock import patch
+
 import pandas as pd
 import pytest
 from sqlalchemy.orm import Session
@@ -39,14 +41,15 @@ class FakeDB(Session):
 @pytest.mark.asyncio
 async def test_predict_endpoint(monkeypatch):
     appointments_json = fake_appointments()
-    monkeypatch.setattr(app, "process_postal_codes", fake_postal_codes)
-    monkeypatch.setattr(app, "load_model", fake_model)
-    monkeypatch.setattr(app, "delete", lambda x: x)
-    # patch create treatment groups and add column to the dataframe
-    monkeypatch.setattr(
-        app, "create_treatment_groups", lambda x, y, z: x.assign(treatment_group=1)
-    )
+    with patch("json.loads", return_value={"name": "test", "value": 123}):
+        monkeypatch.setattr(app, "process_postal_codes", fake_postal_codes)
+        monkeypatch.setattr(app, "load_model", fake_model)
+        monkeypatch.setattr(app, "delete", lambda x: x)
+        # patch create treatment groups and add column to the dataframe
+        monkeypatch.setattr(
+            app, "create_treatment_groups", lambda x, y, z: x.assign(treatment_group=1)
+        )
 
-    output = await predict(appointments_json, "2023-01-05", FakeDB())
-    output_df = pd.DataFrame(output)
-    assert output_df.shape == (4, 15)
+        output = await predict(appointments_json, "2023-01-05", FakeDB())
+        output_df = pd.DataFrame(output)
+        assert output_df.shape == (4, 15)
