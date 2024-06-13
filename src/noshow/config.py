04244d05aedf2ae4e2c1c9acb4824691cca0b1d6
Ruben Peters <r.peters-7@umcutrecht.nl>
2024-06-13 10:55:04 +0200

    Move settings and numbers to config

--- src/noshow/config.py
+++ src/noshow/config.py
@@ -1,1 +1,15 @@
-MUTE_PERIOD = 0
+from pathlib import Path
+
+import tomli
+
+CONFIG_PATH = Path(__file__).parents[2] / "run" / "config" / "config.toml"
+
+with open(CONFIG_PATH, "rb") as f:
+    config = tomli.load(f)
+
+
+MUTE_PERIOD = config["feature_building"]["mute_period"]
+MINUTES_EARLY_CUTOFF = config["feature_building"]["minutes_early_cutoff"]
+APPOINTMENTS_LAST_DAYS = config["feature_building"]["appointments_last_days"]
+
+CLINIC_PHONENUMBERS = config["clinic_phonenumbers"]
