e6de8fe19a9af0886315a055a61e9b45ff7998c5
Ruben Peters <r.peters-7@umcutrecht.nl>
2024-07-17 15:05:42 +0200

    Updated deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
-for VAR in API_KEY APP_ID APP_NAME MANIFEST_FILE
+for VAR in API_KEY ADMIN_DASH_ID ADMIN_DASH_NAME ADMIN_DASH_MANIFEST_FILE DASH_ID DASH_NAME DASH_TEST_ID DASH_TEST_NAME DASH_MANIFEST_FILE API_ID API_NAME API_TEST_ID API_TEST_NAME API_MANIFEST_FILE 
 do
     if [[ -z ${!VAR+x} ]]; then
         echo "env variable" $VAR "missing"
@@ -6,7 +6,38 @@ do
     fi
 done
 
-cp $MANIFEST_FILE manifest.json
+read -p "What do you want to deploy? Options: 'admin-dash'/1 ; 'calling-dash'/2 ; 'calling-dash-test'/3 ; 'api'/4 ; 'api-test'/5 " APPLICATION
+APPLICATION = ${APPLICATION:-N}
+
+if [[ $APPLICATION == 'admin-dash' || $APPLICATION == "1" ]]; then
+    echo "Deploying Admin dashboard"
+    cp $ADMIN_DASH_MANIFEST_FILE manifest.json
+    APP_ID=$ADMIN_DASH_ID
+    APP_NAME=$ADMIN_DASH_NAME
+elif [[ $APPLICATION == 'calling-dash' || $APPLICATION == "2" ]]; then
+    echo "Deploying Calling dashboard production"
+    cp $DASH_MANIFEST_FILE manifest.json
+    APP_ID=$DASH_ID
+    APP_NAME=$DASH_NAME
+elif [[ $APPLICATION == 'calling-dash-test' || $APPLICATION == "3" ]]; then
+    echo "Deploying Calling dashboard test"
+    cp $DASH_MANIFEST_FILE manifest.json
+    APP_ID=$DASH_TEST_ID
+    APP_NAME=$DASH_TEST_NAME
+elif [[ $APPLICATION == 'api' || $APPLICATION == "4" ]]; then
+    echo "Deploying API production"
+    cp $API_MANIFEST_FILE manifest.json
+    APP_ID=$API_ID
+    APP_NAME=$API_NAME
+elif [[ $APPLICATION == 'api-test' || $APPLICATION == "5" ]]; then
+    echo "Deploying API test"
+    cp $API_MANIFEST_FILE manifest.json
+    APP_ID=$API_TEST_ID
+    APP_NAME=$API_TEST_NAME
+else
+    echo "Invalid option"
+    return
+fi
 
 rsconnect deploy manifest \
     --server https://rsc.ds.umcutrecht.nl/ \
