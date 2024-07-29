7d3989ac5f79a005f2741008e85e234ffcc929b9
Ruben Peters <r.peters-7@umcutrecht.nl>
2024-07-29 10:26:10 +0200

    Changes to work with Azure table storage

--- src/noshow/api/app.py
+++ src/noshow/api/app.py
@@ -6,26 +6,16 @@ from pathlib import Path
 from typing import Dict, List, Optional
 
 import tomli
+from azure.data.tables import TableServiceClient
+from azure.identity import DefaultAzureCredential
 from dotenv import load_dotenv
 from fastapi import Depends, FastAPI, HTTPException, Security
 from fastapi.security.api_key import APIKeyHeader
-from sqlalchemy import create_engine
-from sqlalchemy.orm import Session, sessionmaker
 
 from noshow.api.app_helpers import (
-    create_treatment_groups,
-    fix_outdated_appointments,
     load_model,
-    remove_sensitive_info,
 )
 from noshow.config import CLINIC_PHONENUMBERS, KEEP_SENSITIVE_DATA
-from noshow.database.models import (
-    ApiPatient,
-    ApiPrediction,
-    ApiRequest,
-    ApiSensitiveInfo,
-    Base,
-)
 from noshow.model.predict import create_prediction
 from noshow.preprocessing.load_data import (
     load_appointment_json,
@@ -40,30 +30,17 @@ load_dotenv()
 
 app = FastAPI()
 
+credential = DefaultAzureCredential()
+service = TableServiceClient(
+    endpoint="https://noshow01dsa.table.core.windows.net/", credential=credential
+)
+call_predictions_client = service.get_table_client(table_name="callpredictions")
+sensitive_info_client = service.get_table_client(table_name="sensitiveinfo")
+
 with open(Path(__file__).parents[3] / "pyproject.toml", "rb") as f:
     config = tomli.load(f)
 
 API_VERSION = config["project"]["version"]
-
-DB_USER = os.getenv("DB_USER", "")
-DB_PASSWD = os.getenv("DB_PASSWD", "")
-DB_HOST = os.getenv("DB_HOST", "")
-DB_PORT = os.getenv("DB_PORT", 1433)
-DB_DATABASE = os.getenv("DB_DATABASE", "")
-
-if DB_USER == "":
-    print("Using debug SQLite database...")
-    SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
-    execution_options = {"schema_translate_map": {"noshow": None}}
-else:
-    SQLALCHEMY_DATABASE_URL = (
-        rf"mssql+pymssql://{DB_USER}:{DB_PASSWD}@{DB_HOST}:{DB_PORT}/{DB_DATABASE}"
-    )
-    execution_options = None
-
-engine = create_engine(SQLALCHEMY_DATABASE_URL, execution_options=execution_options)
-Base.metadata.create_all(engine)
-SessionLocal = sessionmaker(bind=engine)
 
 api_key_header = APIKeyHeader(name="X-API-KEY", auto_error=False)
 
@@ -77,15 +54,6 @@ def get_bins():
     return fixed_bins
 
 
-def get_db():
-    try:
-        db = SessionLocal()
-        yield db
-        db.close()
-    except Exception as e:
-        print(e)
-
-
 def get_api_key(api_key_header: str = Security(api_key_header)) -> str:
     if api_key_header == os.getenv("X_API_KEY", ""):
         return api_key_header
@@ -97,7 +65,6 @@ def get_api_key(api_key_header: str = Security(api_key_header)) -> str:
 async def predict(
     input: List[Dict],
     start_date: Optional[str] = None,
-    db: Session = Depends(get_db),
     api_key: str = Depends(get_api_key),
 ) -> List[Dict]:
     """
@@ -140,85 +107,52 @@ async def predict(
         "prediction", ascending=False
     ).reset_index()
 
-    prediction_df = create_treatment_groups(prediction_df, db, get_bins())
+    # TODO better assign treatment group
+    prediction_df["treatment_group"] = 2
 
-    remove_sensitive_info(db, start_time, lookback_days=KEEP_SENSITIVE_DATA)
-
-    end_time = datetime.now()
-    apirequest = ApiRequest(
-        timestamp=start_time,
-        api_version=API_VERSION,
-        response_code=200,
-        response_message="success",
-        endpoint="predict",
-        runtime=(end_time - start_time).total_seconds(),
-    )
-    db.add(apirequest)
-
+    # Store pseudo_id, prediction in azure table storage
     for _, row in prediction_df.iterrows():
-        apisensitive = db.get(ApiSensitiveInfo, row["pseudo_id"])
+        entity = {
+            "PartitionKey": row["pseudo_id"],
+            "RowKey": row["APP_ID"],
+            "start": str(row["start"]),
+            "prediction": str(row["prediction"]),
+            "treatment_group": str(row["treatment_group"]),
+            "api_version": API_VERSION,
+            "response_code": 200,
+            "response_message": "success",
+            "endpoint": "predict",
+            "runtime": (datetime.now() - start_time).total_seconds(),
+        }
+        call_predictions_client.upsert_entity(entity=entity)
+        sensitive_entity = {
+            "PartitionKey": row["pseudo_id"],
+            "RowKey": row["APP_ID"],
+            "name_text": row["name_text"],
+            "name_given1_callMe": row["name_given1_callMe"],
+            "birthDate": str(row["birthDate"]),
+            "telecom1_value": row["telecom1_value"],
+            "telecom2_value": row["telecom2_value"],
+            "telecom3_value": row["telecom3_value"],
+        }
+        sensitive_info_client.upsert_entity(entity=sensitive_entity)
 
-        if not apisensitive:
-            if row["name_text"] is None:
-                row["name_text"] = ""
-                logger.warning(
-                    f"Patient {row['pseudo_id']} has no name_text, "
-                    "replacing with empty string"
-                )
+    # Get entities from azure table storage that have a start date more than 7 days ago
+    # TODO gebruik LOOKBACK_DAYS
+    deletable_entities = call_predictions_client.query_entities(
+        query_filter=f"start lt '{start_date}'"
+    )
+    # retrieve partitionkey and rowkey from entities
+    entities = [
+        {"PartitionKey": entity["PartitionKey"], "RowKey": entity["RowKey"]}
+        for entity in deletable_entities
+    ]
+    # delete entities from azure table storage
+    for entity in entities:
+        sensitive_info_client.delete_entity(entity["PartitionKey"], entity["RowKey"])
 
-            apisensitive = ApiSensitiveInfo(
-                patient_id=row["pseudo_id"],
-                full_name=row["name_text"],
-                first_name=row["name_given1_callMe"],
-                birth_date=row["birthDate"],
-                mobile_phone=row["telecom1_value"],
-                home_phone=row["telecom2_value"],
-                other_phone=row["telecom3_value"],
-            )
-        else:
-            # name and birthdate can't change, but phone number might
-            apisensitive.mobile_phone = row["telecom1_value"]
-            apisensitive.home_phone = row["telecom2_value"]
-            apisensitive.other_phone = row["telecom3_value"]
-
-        apipatient = db.get(ApiPatient, row["pseudo_id"])
-        if not apipatient:
-            apipatient = ApiPatient(
-                id=row["pseudo_id"],
-            )
-        apipatient.treatment_group = int(row["treatment_group"])
-        apiprediction = db.get(ApiPrediction, row["APP_ID"])
-        if not apiprediction:
-            apiprediction = ApiPrediction(
-                id=row["APP_ID"],
-                patient_id=row["pseudo_id"],
-                prediction=row["prediction"],
-                start_time=row["start"],
-                request_relation=apirequest,
-                patient_relation=apipatient,
-                clinic_name=row["hoofdagenda"],
-                clinic_reception=row["description"],
-                clinic_phone_number=CLINIC_PHONENUMBERS.get(row["hoofdagenda"], ""),
-                active=True,
-            )
-        else:
-            # All values of a prediction can be updated except the ID and treatment
-            apiprediction.prediction = row["prediction"]
-            apiprediction.start_time = row["start"]
-            apiprediction.request_relation = apirequest
-            apiprediction.clinic_name = row["hoofdagenda"]
-            apiprediction.clinic_reception = row["description"]
-            apiprediction.clinic_phone_number = CLINIC_PHONENUMBERS.get(
-                row["hoofdagenda"], ""
-            )
-            apiprediction.active = True
-
-        db.merge(apisensitive)
-        db.merge(apiprediction)
-        db.merge(apipatient)
-        db.commit()
-
-    fix_outdated_appointments(db, prediction_df["APP_ID"], start_date)
+    # TODO: fix outdated
+    # fix_outdated_appointments(db, prediction_df["APP_ID"], start_date)
 
     return prediction_df.reset_index().to_dict(orient="records")
 
